5650f903c4715c238a1c47bbf9dc87e1fb3de56e
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-06-07 11:02:41 -0700

    basic webpack babel build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
