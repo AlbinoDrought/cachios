f9e8897c9ab2edf39e00d137b2ca65bfa1c83ab4
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-06-07 13:59:54 -0700

    ignore index.js for coverage when testing, fix style

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const instance = new Cachios(axios);
 // allow for similar axios syntax
 instance.create = function create(axiosInstance, nodeCacheConf) {
   return new Cachios(axiosInstance, nodeCacheConf);
-}
+};
 
 module.exports = instance;
 module.exports.default = instance;
--- tests/index.js
+++ tests/index.js
@@ -2,8 +2,8 @@
 const testsContext = require.context('./', true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
 
-// require all src files except main.js for coverage.
+// require all src files except index.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context('../src', true, /^\.\/(?!main(\.js)?$)/);
+const srcContext = require.context('../src', true, /^\.\/(?!index(\.js)?$)/);
 srcContext.keys().forEach(srcContext);
